a9795f4c08f3ec4130da4ff1a469186ae9c7d9db
kenshirokk <iamkenshiro@live.cn>
2017-11-24 23:23:26 +0800

    哈哈

--- src/main/java/com/kenshiro/instagram/controller/IndexController.java
+++ src/main/java/com/kenshiro/instagram/controller/IndexController.java
@@ -1,13 +1,19 @@
 package com.kenshiro.instagram.controller;
 
+import com.kenshiro.instagram.document.Node;
 import com.kenshiro.instagram.document.User;
 import com.kenshiro.instagram.job.Job;
+import com.kenshiro.instagram.repository.NodeRepository;
 import com.kenshiro.instagram.service.InstagramService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.io.IOException;
@@ -19,6 +25,8 @@ public class IndexController {
 
 	@Autowired
 	private InstagramService instagramService;
+	@Autowired
+	private NodeRepository nodeRepository;
 	@Autowired
 	private Job job;
 
@@ -47,4 +55,24 @@ public class IndexController {
 	public void fetchToMongo() {
 		job.fetchToMongo();
 	}
+
+	@GetMapping("recent")
+	public String recent(Model model, @RequestParam(required = false, defaultValue = "1") int page) {
+
+		PageRequest request = PageRequest.of(page - 1, 12, Sort.Direction.DESC, "date");
+        List<User> users = instagramService.findAllUser();
+//        Page<Node> all = nodeRepository.findAll(request);
+        Page<Node> all = nodeRepository.findByIsVideoIsFalse(request);
+        model.addAttribute("users", users);
+        model.addAttribute("nodes", all.getContent());
+        return "index";
+	}
+
+	@RequestMapping("loadMore")
+    @ResponseBody
+	public List<Node> loadMore(int page) {
+        PageRequest request = PageRequest.of(page-1, 12, Sort.Direction.DESC, "date");
+        Page<Node> all = nodeRepository.findByIsVideoIsFalse(request);
+        return all.getContent();
+    }
 }
--- src/main/java/com/kenshiro/instagram/repository/NodeRepository.java
+++ src/main/java/com/kenshiro/instagram/repository/NodeRepository.java
@@ -1,6 +1,8 @@
 package com.kenshiro.instagram.repository;
 
 import com.kenshiro.instagram.document.Node;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
@@ -10,4 +12,5 @@ import java.util.List;
 public interface NodeRepository extends MongoRepository<Node, String> {
     Node findByNodeId(String nodeId);
     List<Node> findByDownloadedExistsOrDownloadedIsFalse(boolean exists);
+    Page<Node> findByIsVideoIsFalse(Pageable pageable);
 }
