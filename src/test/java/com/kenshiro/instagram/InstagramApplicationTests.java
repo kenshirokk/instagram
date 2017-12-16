734cce3d03151edd0855f13fdbda24bc3b8c38d2
kenshirokk <iamkenshiro@live.cn>
2017-12-16 15:36:29 +0800

    #

--- src/test/java/com/kenshiro/instagram/InstagramApplicationTests.java
+++ src/test/java/com/kenshiro/instagram/InstagramApplicationTests.java
@@ -5,20 +5,17 @@ import com.kenshiro.instagram.document.User;
 import com.kenshiro.instagram.repository.NodeRepository;
 import com.kenshiro.instagram.service.InstagramService;
 import com.mongodb.client.result.UpdateResult;
-import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.core.query.Criteria;
 import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.data.mongodb.core.query.Update;
-import org.springframework.test.context.junit4.SpringRunner;
 
 import java.io.IOException;
 import java.util.List;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest
+//@RunWith(SpringRunner.class)
+//@SpringBootTest
 public class InstagramApplicationTests {
 
 	@Autowired
